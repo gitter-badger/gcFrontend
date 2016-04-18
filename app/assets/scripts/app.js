ccc95f0154831a70050f723371a1742d17bee0a3
Dominik Biedebach <dominik.biedebach@gmail.com>
2016-04-18 10:16:09 +0200

    Added Actions to single server page

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,7 @@
 // Controller
 controllers = {
-  home: require('./controller/home')
+  home: require('./controller/home'),
+  server: require('./controller/server')
 };
 
 // Router
@@ -11,6 +12,7 @@ var app = angular.module('panelApp', [
 ]);
 
 app.controller('homeController', ['$scope', '$http', function($scope, $http) { controllers.home($scope, $http) }]);
+app.controller('serverController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) { controllers.server($scope, $http, $routeParams) }]);
 
 app.config(['$routeProvider', function($routeProvider) {
   router($routeProvider);
--- build/assets/scripts/app.js
+++ build/assets/scripts/app.js
@@ -1,7 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // Controller
 controllers = {
-  home: require('./controller/home')
+  home: require('./controller/home'),
+  server: require('./controller/server')
 };
 
 // Router
@@ -12,12 +13,13 @@ var app = angular.module('panelApp', [
 ]);
 
 app.controller('homeController', ['$scope', '$http', function($scope, $http) { controllers.home($scope, $http) }]);
+app.controller('serverController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) { controllers.server($scope, $http, $routeParams) }]);
 
 app.config(['$routeProvider', function($routeProvider) {
   router($routeProvider);
 }]);
 
-},{"./controller/home":2,"./router":3}],2:[function(require,module,exports){
+},{"./controller/home":2,"./controller/server":3,"./router":4}],2:[function(require,module,exports){
 module.exports = function($scope, $http) {
   $http.get('http://176.9.19.16:8080/api/library').success(function(data) {
     console.log(data);
@@ -26,6 +28,15 @@ module.exports = function($scope, $http) {
 };
 
 },{}],3:[function(require,module,exports){
+module.exports = function($scope, $http, $routeParams) {
+  serverID = $routeParams.id;
+  $http.get('http://176.9.19.16:8080/api/library/' + serverID).success(function(data) {
+    $scope.server = data;
+    console.log($scope.server);
+  });
+};
+
+},{}],4:[function(require,module,exports){
 module.exports = function($routeProvider) {
   $routeProvider.
     when('/', {

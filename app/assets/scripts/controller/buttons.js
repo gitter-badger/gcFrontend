728cc4b8c3c2051d5379aab46198b7c693d90776
koriwi <kilian2789@gmail.com>
2016-04-21 02:32:10 +0200

    http request for each button

--- app/assets/scripts/controller/buttons.js
+++ app/assets/scripts/controller/buttons.js
@@ -1,5 +1,17 @@
-module.exports = function($scope,$html) {
-  $scope.click = function(e){
-  	console.log(e);
+module.exports = function($scope,$http) {
+  $scope.click = function(server,command){
+  	$http({
+	    method: 'POST',
+	    url: 'http://176.9.19.16',
+	    data:
+	    {
+	  		"id":server,
+	  		"action":command
+	  	},
+	    headers: {'Content-Type': 'application/json'}
+	}).then(function(e){
+  		console.log(e);
+  	});
+  	console.log(server + ";"+ command);
   }
 }
